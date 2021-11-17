ca9b219559a91bb4e0157ec3023de7709892c735
eilerglen <83466883+eilerglen@users.noreply.github.com>
2021-11-18 02:17:39 +0300

    Еще небольшие правки из можно лучше

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
-
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./components/app/app";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
