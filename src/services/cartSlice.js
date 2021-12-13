c4765feab43f8867a0060cea7c21ca273160de3a
eilerglen <83466883+eilerglen@users.noreply.github.com>
2021-12-13 12:31:57 +0300

    all slices added

---
+++ src/services/cartSlice.js
@@ -0,0 +1,59 @@
+import { createSlice } from '@reduxjs/toolkit';
+const initialState = {
+    sortedCart: {
+        bun: {},
+        fillers: [],
+    },
+    itemsOrder: [],
+    counts: {},
+    total: 0,
+}
+
+export const carSlice = createSlice({
+    name: 'cart',
+    initialState,
+    reducers: {
+        addIngredients: (state, action) => {
+            const {item} = action.payload;
+            if(!state.counts[item._id]) {
+                state.counts[item._id] = 1;
+            } else {
+                state.counts[item._id]++
+            }
+
+            if(item.type === 'bun') {
+                const bunId = state.sortedCart.bun._id;
+                state.counts[bunId] && state.counts[bunId]--
+                state.sortedCart.bun = item;
+            } else {
+                const newFillers = [...state.sortedCart.fillers, {item, constructorId: nanoid()}]
+                state.sortedCart.fillers = newFillers;
+            }
+            
+            state.itemsToOrder = state.sortedCart.bun._id ? state.sortedCart.fillers.map(el => el.item._id).concat([state.sortedCart.bun._id]) : state.sortedCart.fillers.map(el => el.item._id)
+
+        },
+        deleteIngredient: (state, action) => {
+            const {id, itemImdex} = action.payload;
+            const newFillers = [...state.sortedCart.fillers];
+            newFillers.splice(itemIndex, 1);
+            state.counts[id]--;
+            state.sortedCart.fillers = newFillers;
+            state.itemsToOrder = state.sortedCart.fillers.map(el => el.item?._id).concat([state.sortedCart.bun._id])
+        },
+        resetCart: (state) => state = initialState,
+        moveIngredient: (state, action) => {
+    
+          const { dragIndex, dropIndex } = action.payload;
+          [state.sortedCart.fillers[dragIndex], state.sortedCart.fillers[dropIndex]] = [state.sortedCart.fillers[dropIndex], state.sortedCart.fillers[dragIndex]];  
+    },
+    countTotal: (state) => {
+        const fillers = state.sortedCart.fillers;
+        state.total = state.sortedCart.bun.price 
+        ? (fillers.reduce((acc, p) => acc + p.item.price, 0) + state.sortedCart.bun.price * 2)
+        : (fillers.reduce((acc, p) => acc + p.item.price, 0));
+      }
+    },  
+})
+export const { addIngredient, deleteIngredient, resetCart, countTotal, moveIngredient  } = cartSlice.actions;
+export default cartSlice.reducer;
---
+++ src/services/orderSlice.js
@@ -0,0 +1,62 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { API } from "components/app/app";
+
+export const setOrder = createAsyncThunk(
+    'order/setOrder',
+    async (ids) => {
+        try {
+            const response = await fetch (`${API}/orders`, {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    ingredients: ids
+                })
+            })
+            if (!response.ok) {
+                throw new Error('Fetch error')
+            }
+            const order = await response.json()
+            return order;
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+);
+
+export const orderSlice = createSlice ({
+    name: 'order',
+    initialState:  {
+        order: {},
+        isLoading: false,
+        hasError: false
+    },
+    reducers: {
+        clearOrder: (state) => {
+            state.order = {}
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+        .addCase(setOrder.pending, (state) => {
+            state.isLoading = true;
+            state.hasError = false;
+        })
+        .addCase(setOrder.fulfilled, (state, action) => {
+            state.order = action.payload;
+            state.isLoading = false;
+            state.hasError = false;
+        })
+        .addCase(setOrder.rejected, (state) => {
+            state.isLoading = false;
+            state.hasError = true;
+            clearOrder()
+          })
+    }
+})
+
+export const { clearOrder } = orderSlice.actions;
+export default orderSlice.reducer;
