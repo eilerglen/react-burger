c4c77bf433ed0358422974ffb2e037d661f83a5a
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-01-24 23:07:52 +0300

    fix of router on logib

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -8,36 +8,79 @@ import { useLocation, Redirect, useHistory } from 'react-router-dom';
 import { TLocationState } from '../../types/types';
 
 const RegisterPage = () => {
-  const [isAuthorised, isLoading, hasError] = useAppSelector(store => store.login)
-  console.log(isAuthorised, isLoading)
-  const location = useLocation()
+  const { isAuthorized, isLoading, hasError } = useAppSelector(store => store.auth)
+  const location = useLocation();
   const history = useHistory()
+
   const [isVisible, setVisible] = React.useState(false)
   const [form, setForm] = React.useState({ name: '', email: '', password: '' });
+  const dispatch = useAppDispatch();
 
   const onChange = (e) => {
-    setForm({ ...form, [e.target.name]: e.target.value });
-  }
-
+      setForm({ ...form, [e.target.name]: e.target.value });
+  };
   const onSubmit = async (e) => {
-    e.preventDefault();
-    let res = await dispatch(register(form))
-    console.log(res)
-    if (!isLoading && !hasError) {
-        setForm({ name: '', email: '', password: '' })
-        return history.replace('/login');
-    }
+      e.preventDefault();
+      let res = await dispatch(register(form))
+      console.log(res)
+      if (!isLoading && !hasError) {
+          setForm({ name: '', email: '', password: '' })
+          return history.replace('/login');
+      }
   }
+  if (isAuthorized) {
+      console.log('in name ', location.state)
+      const { from } = location.state || { from: { pathname: '/' } }
+      return (
+          <Redirect to={from} />
+      )
+  }
+  return (
+      <>
+      {!isLoading && (
+      <div className={styles.wrapper}>
+          <form className={styles.form} onSubmit={onSubmit}>
+              <h1 className={styles.heading}>Регистрация</h1>
+              <Input
+                  type="text"
+                  placeholder="Имя"
+                  name="name"
+                  value={form.name}
+                  onChange={onChange}
+              />
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    let res = await dispatch(register(form))
-    console.log(res)
-    if (!isLoading && !hasError) {
-        setForm({ name: '', email: '', password: '' })
-        return history.replace('/login');
-    }
+              <Input
+                  type="email"
+                  placeholder="Email"
+                  name="email"
+                  value={form.email}
+                  onChange={onChange}
+              />
+
+              <Input
+                  type={isVisible ? 'text' : "password"}
+                  placeholder="Пароль"
+                  name="password"
+                  icon={isVisible ? 'ShowIcon' : 'HideIcon'}
+                  value={form.password}
+                  onChange={onChange}
+                  onIconClick={() => setVisible(!isVisible)}
+              />
+
+              <span className={styles.button}>
+                  <Button>Зарегистрироваться</Button>
+              </span>
+          </form>
+          <p className={styles.text}>
+              Уже зарегистрированы?&nbsp;
+              <Link className={styles.link} to={'/login'}>
+                  Войти
+              </Link>
+          </p>
+      </div>
+      )}
+  </>
+  );
 }
 
-}
-export default RegisterPage
+export default RegisterPage;
