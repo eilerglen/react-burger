beee9eb06a4ff06e1316e7ae3ec369f80c5b6bf4
eilerglen <83466883+eilerglen@users.noreply.github.com>
2022-01-27 17:18:07 +0300

    IngredientPage is done!

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -5,19 +5,21 @@ import NotFound404 from '../../pages/not-found-404/not-found-404'
 import Profile from '../../pages/profile/profile'
 import ForgotPassword from '../../pages/forgot-password/forgot-password'
 import ResetPassword from '../../pages/reset-password/reset-password'
-import IngredientPage from '../../pages/ingredient/ingredient'
+import IngredientPage from '../../pages/ingredient-page/ingredient-page'
 import { useAppDispatch, useAppSelector } from '../../services/hooks';
 import {Route, Switch, useLocation, useHistory} from 'react-router-dom'
 import AppHeader from '../app-header/app-header';
 import { FC, useEffect } from 'react';
 import { TLocationState } from '../../types/types';
 import { getIngredients } from '../../services/ingredientsSlice';
+import Spinner from '../spinner/spinner'
 
 
 const App: FC = () => {
   const dispatch = useAppDispatch()
   const location = useLocation<TLocationState>()
   const history = useHistory()
+  const { isLoading } = useAppSelector(store => store.auth)
   const isPush = history.action === 'PUSH'
   let pushLocation = isPush && location.state && location.state.pushLocation
 
@@ -25,9 +27,14 @@ const App: FC = () => {
     dispatch(getIngredients())
   },[dispatch])
 
+  const closeModal = () => {
+
+  }
   return (
+   
     <>
-      <AppHeader />
+     {isLoading && <Spinner />}
+       <AppHeader />
       <Switch location = {pushLocation || location }>
         <Route path='/' exact = {true}>
           <HomePage />
@@ -47,10 +54,23 @@ const App: FC = () => {
         <Route path='/reset-password' exact>
           <ResetPassword />
         </Route>
-        <Route path = '/ingredients/:id'></Route>
-      </Switch>  
+        <Route path='/ingredients/:id' exact>
+          <IngredientPage />
+        </Route>
+        {/* <Route>
+          <NotFound404 />
+        </Route> */}
+      </Switch> 
+      {/* {pushLocation && (
+        <Route path='/ingredients/:id'>
+          <Modal title='Детали ингредиента' onClose={closeModal}>
+            <IngredientDetails />
+          </Modal>
+        </Route>
+      )} */}
     </>
-  );
+  )
+   
 }
 
 export default App;
--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,30 +1,36 @@
 import detailsModalStyles from './ingredient-details.module.css';
 import NutritionList from '../nutrition-list/nutrition-list';
-import { useAppSelector } from '../../services/hooks';
+import { useAppSelector, useAppParams } from '../../services/hooks';
 import Spinner from '../spinner/spinner'
-import {FC} from 'react'
 
-const IngredientDetails: FC = () => {
-    const  { ingredientShow } = useAppSelector(store => store.ingredientDetails)
-    const { isLoading, hasError} = useAppSelector(store => store.ingredients)
 
+const IngredientDetails = () => {
+    const { id } = useAppParams();
+    const { ingredients, isLoading, hasError } = useAppSelector(store => store.ingredients);
+    const {ingredientShow} = useAppSelector(store => store.ingredientDetails);
+    
+    let activeIngredient;
+    if (id) {
+        activeIngredient = ingredients.find(item => item._id === id)
+    } else {
+        activeIngredient = ingredientShow
+    }
     return (
         <>
-            {!isLoading && !hasError && ingredientShow !== null ? (
-            <div className={detailsModalStyles.detail}>
-            <picture className={detailsModalStyles.picture}>
-                <source media="(max-width: 768px)" srcSet={ingredientShow.image_mobile} />
-                <source media="(min-width: 1024px)" srcSet={ingredientShow.image_large} />
-                <img src={ingredientShow.image} alt={ingredientShow.name} className={detailsModalStyles.image} />
-            </picture>
-            <h3 className={detailsModalStyles.title}>{ingredientShow.name}</h3>
-            <NutritionList {...ingredientShow} />
-            </div>
+            {!isLoading && !hasError && ingredients.length !== 0 ? (
+                        <div className={detailsModalStyles.detail}>
+                        <picture className={detailsModalStyles.picture}>
+                            <source media="(max-width: 768px)" srcSet={activeIngredient?.image_mobile} />
+                            <source media="(min-width: 1024px)" srcSet={activeIngredient?.image_large} />
+                            <img src={activeIngredient?.image} alt={activeIngredient?.name} className={detailsModalStyles.image} />
+                        </picture>
+                        <h3 className={detailsModalStyles.title}>{activeIngredient?.name}</h3>
+                        <NutritionList {...activeIngredient} />
+                    </div>
             ) : (<Spinner />)
             }
         </>
-        )
-    }
+
+    )
+}
 export default IngredientDetails;
-
-
